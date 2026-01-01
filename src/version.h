902a796ec64ddf95ce5bcaf000e8534b43893eba
Nicolas Philippe <nicolas.philippe@me.com>
2026-01-01 13:48:58 +0100

    chore: mise à jour version 2.1.0

--- src/version.h
+++ src/version.h
@@ -1,9 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-// Format de version: ANNÉE.MOIS.RÉVISION
-// Exemple: 2025.1.1 = Janvier 2025, révision 1
-#define FIRMWARE_VERSION "2025.12.21"
+// Format de version: MAJEUR.MINEUR.PATCH
+// Exemple: 2.1.0 = version 2.1, patch 0
+#define FIRMWARE_VERSION "2.1.0"
 #define FIRMWARE_BUILD_DATE __DATE__
 #define FIRMWARE_BUILD_TIME __TIME__
 
